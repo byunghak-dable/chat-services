42ea618989e80c6ace0d0b9c432c34d709621652
weed082 <forsythia3240@gmail.com>
2022-11-20 12:18:22 +0900

    [UPDATE] add redis

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/weed082/chat-server/internal/adapter/driven/repository"
 	"github.com/weed082/chat-server/internal/adapter/driven/repository/mysql"
+	"github.com/weed082/chat-server/internal/adapter/driven/repository/redis"
 	"github.com/weed082/chat-server/internal/adapter/driver/rest"
 	"github.com/weed082/chat-server/internal/application"
 )
@@ -18,7 +19,8 @@ var logger = log.New(os.Stdout, "LOG", log.LstdFlags|log.Llongfile)
 
 var (
 	restServer *rest.Rest
-	db         *mysql.Mysql
+	mysqlDb    *mysql.Mysql
+	redisDb    *redis.Redis
 )
 
 func init() {
@@ -26,8 +28,12 @@ func init() {
 	if err != nil {
 		log.Fatalf("error loading .env file")
 	}
+	// init mysql
 	sqlDsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT"), os.Getenv("MYSQL_DATABASE"))
-	db = mysql.New(logger, sqlDsn)
+	mysqlDb = mysql.New(logger, sqlDsn)
+	// init redis
+	redisHost := fmt.Sprintf("%s:%s", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"))
+	redisDb = redis.New(logger, redisHost, os.Getenv("REDIS_PASSWORD"), 0)
 }
 
 func main() {
@@ -36,14 +42,14 @@ func main() {
 }
 
 func runRest() {
-	userRepo := repository.NewUserRepo(logger, db)
+	userRepo := repository.NewUserRepo(logger, mysqlDb)
 	userApp := application.NewUserApp(logger, userRepo)
 	restServer = rest.New(logger, userApp)
 	go restServer.Run(os.Getenv("REST_PORT"))
 }
 
 func gracefulShutdown() {
-	defer db.Close()
+	defer mysqlDb.Close()
 	defer restServer.Stop()
 
 	terminationChan := make(chan os.Signal, 1)
--- internal/adapter/driven/repository/redis/redis.go
+++ internal/adapter/driven/repository/redis/redis.go
@@ -1,13 +1,28 @@
 package redis
 
-import "log"
+import (
+	"context"
+	"log"
+
+	"github.com/go-redis/redis/v9"
+)
 
 type Redis struct {
 	logger *log.Logger
+	*redis.Client
+	ctx context.Context
 }
 
-func New(logger *log.Logger) *Redis {
+func New(logger *log.Logger, address, password string, db int) *Redis {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     address,
+		Password: password,
+		DB:       db, // use default DB
+	})
+
 	return &Redis{
 		logger: logger,
+		Client: rdb,
+		ctx:    context.Background(),
 	}
 }
