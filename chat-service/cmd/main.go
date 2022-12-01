f71875f7085c7728f15582fa1703efc9ca8e5fc2
widcraft <forsythia3240@gmail.com>
2022-12-01 23:04:56 +0900

    [UPDATE] add graceful shutdown

--- chat-service/cmd/main.go
+++ chat-service/cmd/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"reflect"
+	"sync"
+	"syscall"
+
+	"github.com/widcraft/chat-service/external/workerpool"
+)
+
+var (
+	wg       = &sync.WaitGroup{}
+	chatPool = workerpool.New(wg, 1)
+)
 
 func main() {
 	fmt.Println("chat service main")
+}
+
+func gracefulShutdown() {
+	// waiting
+	defer wg.Wait()
+	// closing
+	defer shutdown(chatPool)
+
+	terminationChan := make(chan os.Signal, 1)
+	signal.Notify(terminationChan, os.Interrupt, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
+	<-terminationChan
+}
+
+func shutdown(targets ...interface{ Close() }) {
+	for _, target := range targets {
+		if !reflect.ValueOf(target).IsNil() {
+			target.Close()
+		}
+	}
 }
