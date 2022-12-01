0054b043ff1f79a778b4ea2da239a29a01b68e35
widcraft <forsythia3240@gmail.com>
2022-12-01 22:50:55 +0900

    [UPDATE] add chat app components

--- chat-service/internal/application/chat_app.go
+++ chat-service/internal/application/chat_app.go
@@ -1,18 +1,38 @@
 package application
 
-type participant interface {
+type user interface {
 	GetIdx() uint
 	GetName() string
 	GetImageUrl() string
 	Send() error
 }
 
-type ChatApp struct {
-	chatRoom map[int][]participant
+type workerPool interface {
+	RegisterJob(func())
 }
 
-func NewChatApp() *ChatApp {
+type ChatApp struct {
+	chatRoom map[int][]user
+	pool     workerPool
+	poolChan chan error
+}
+
+func NewChatApp(pool workerPool) *ChatApp {
 	return &ChatApp{
-		chatRoom: make(map[int][]participant),
+		chatRoom: make(map[int][]user),
+		pool:     pool,
+		poolChan: make(chan error),
 	}
+}
+
+func CreateRoom() error {
+	return nil
+}
+
+func JoinRoom() error {
+	return nil
+}
+
+func SendMessage() error {
+	return nil
 }
