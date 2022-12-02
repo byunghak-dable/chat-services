741c9ff34805f3c44581f3d7a69c4818e113d6f0
widcraft <forsythia3240@gmail.com>
2022-12-03 00:45:33 +0900

    [UPDATE] add chat app functionality

--- chat-service/internal/application/chat_app.go
+++ chat-service/internal/application/chat_app.go
@@ -1,5 +1,11 @@
 package application
 
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/widcraft/chat-service/internal/domain/dto"
+	"github.com/widcraft/chat-service/port"
+)
+
 type user interface {
 	GetIdx() uint
 	GetName() string
@@ -12,27 +18,39 @@ type workerPool interface {
 }
 
 type ChatApp struct {
+	logger   *log.Logger
 	pool     workerPool
 	poolChan chan error
 	chatRoom map[int][]user
+	repo     port.ChatRepository
 }
 
-func NewChatApp(pool workerPool) *ChatApp {
+func NewChatApp(logger *log.Logger, pool workerPool, repo port.ChatRepository) *ChatApp {
 	return &ChatApp{
+		logger:   logger,
 		pool:     pool,
 		poolChan: make(chan error),
 		chatRoom: make(map[int][]user),
+		repo:     repo,
 	}
 }
 
-func CreateRoom() error {
+func (app *ChatApp) CreateRoom(name string) (uint, error) {
+	return 0, nil
+}
+
+func (app *ChatApp) ConnectRoom(roomIdx, userIdx uint) error {
 	return nil
 }
 
-func JoinRoom() error {
+func (app *ChatApp) JoinRoom(roomIdx uint) error {
 	return nil
 }
 
-func SendMessage() error {
+func (app *ChatApp) SendMessge(roomIdx uint, message string) error {
 	return nil
+}
+
+func (app *ChatApp) GetMessages(roomIdx uint) ([]dto.MessageDto, error) {
+	return nil, nil
 }
