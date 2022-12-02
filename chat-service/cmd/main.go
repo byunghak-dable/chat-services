0deadb093c2b162f853ad8df55dff91fc9a6ddb5
widcraft <forsythia3240@gmail.com>
2022-12-03 00:44:06 +0900

    [UPDATE] run websocket server on start

--- chat-service/cmd/main.go
+++ chat-service/cmd/main.go
@@ -1,25 +1,62 @@
 package main
 
 import (
-	"fmt"
+	"net"
 	"os"
 	"os/signal"
 	"reflect"
 	"sync"
 	"syscall"
 
+	"github.com/joho/godotenv"
 	log "github.com/sirupsen/logrus"
 	"github.com/widcraft/chat-service/external/workerpool"
+	"github.com/widcraft/chat-service/internal/adapter/repository"
+	"github.com/widcraft/chat-service/internal/adapter/repository/redis"
+	"github.com/widcraft/chat-service/internal/adapter/websocket"
+	"github.com/widcraft/chat-service/internal/application"
 )
 
 var logger = log.New()
+var redisDb *redis.Redis
 var (
 	wg       = &sync.WaitGroup{}
 	chatPool = workerpool.New(wg, 1)
 )
 
+var (
+	ws *websocket.Websocket
+)
+
+// env
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("error loading .env file")
+	}
+}
+
+// DB
+func init() {
+	var err error
+	redisDb, err = redis.New(logger, net.JoinHostPort(os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")), os.Getenv("REDIS_PASSWORD"), 0)
+
+	if err != nil {
+		shutdown(redisDb)
+		logger.Fatalf("redis connection failure: %s", err)
+	}
+}
+
+// servers
+func init() {
+	chatRepo := repository.NewChatRepository(logger, redisDb)
+	chatApp := application.NewChatApp(logger, chatPool, chatRepo)
+	ws = websocket.New(logger, chatApp)
+}
+
 func main() {
-	fmt.Println("chat service main")
+	defer gracefulShutdown()
+	go ws.Run(os.Getenv("WS_PORT"))
 }
 
 func gracefulShutdown() {
