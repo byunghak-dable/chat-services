9320fefcd5566699fd5b3c0a8e874415a1922b43
widcraft <forsythia3240@gmail.com>
2022-12-02 19:58:54 +0900

    [UPDATE] add rest dependencies

--- chat-service/internal/adapter/rest/rest.go
+++ chat-service/internal/adapter/rest/rest.go
@@ -1,8 +1,22 @@
 package rest
 
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/widcraft/chat-service/port"
+)
+
 type Rest struct {
+	logger  *log.Logger
+	chatApp port.ChatApp
 }
 
-func NewRest() *Rest {
-	return &Rest{}
+func NewRest(logger *log.Logger, chatApp port.ChatApp) *Rest {
+	return &Rest{
+		logger:  logger,
+		chatApp: chatApp,
+	}
+}
+
+func (rest *Rest) Close() {
+	// need to close websocket gracefully
 }
