7aaba0f0914fb97f4c40e765e958b1038f4b9aaa
widcraft <forsythia3240@gmail.com>
2022-12-03 00:44:31 +0900

    [UPDATE] add redis for chat repository

--- chat-service/internal/adapter/repository/chat_repository.go
+++ chat-service/internal/adapter/repository/chat_repository.go
@@ -1,8 +1,35 @@
 package repository
 
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/widcraft/chat-service/internal/adapter/repository/redis"
+	"github.com/widcraft/chat-service/internal/domain/entity"
+)
+
 type ChatRepository struct {
+	logger *log.Logger
+	db     *redis.Redis
 }
 
-func NewChatRepository() *ChatRepository {
-	return &ChatRepository{}
+func NewChatRepository(logger *log.Logger, redis *redis.Redis) *ChatRepository {
+	return &ChatRepository{
+		logger: logger,
+		db:     redis,
+	}
+}
+
+func (repo *ChatRepository) CreateRoom(room *entity.Room) error {
+	return nil
+}
+
+func (repo *ChatRepository) JoinRoom(roomIdx, userIdx uint) error {
+	return nil
+}
+
+func (repo *ChatRepository) SaveMessage(userIdx uint, message string) error {
+	return nil
+}
+
+func (repo *ChatRepository) GetMessages(roomIdx uint) ([]entity.Message, error) {
+	return nil, nil
 }
