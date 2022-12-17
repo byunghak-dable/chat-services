f63a74b0225e32a8630d30d7dccfb679fd65fe47
widcraft <forsythia3240@gmail.com>
2022-12-17 18:03:03 +0900

    [SORT]

--- chat-service/internal/adapter/grpc/chat/server.go
+++ chat-service/internal/adapter/grpc/chat/server.go
@@ -5,6 +5,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	"github.com/widcraft/chat-service/internal/adapter/grpc/chat/pb"
+	"github.com/widcraft/chat-service/internal/domain/dto"
 	"github.com/widcraft/chat-service/internal/port"
 )
 
@@ -22,17 +23,19 @@ func New(logger *log.Logger, app port.ChatApp) *Server {
 }
 
 func (s *Server) Connect(stream pb.Chat_ConnectServer) error {
+	var c *client
+	var roomIdx uint
 	for {
 		req, err := stream.Recv()
 		if err != nil {
 			return err
 		}
-		var c *client
 		switch payload := req.GetType().(type) {
 		case *pb.ChatReq_Join:
 			if err = s.join(payload.Join, c, stream); err != nil {
 				return err
 			}
+			roomIdx = uint(payload.Join.RoomIdx)
 			defer func() {
 				if err = s.app.Disconnect(uint(payload.Join.RoomIdx), c); err != nil {
 					s.logger.Errorf("disconnect client failed: %s", err)
@@ -58,6 +61,12 @@ func (s *Server) join(payload *pb.JoinReq, c *client, stream pb.Chat_ConnectServ
 	return nil
 }
 
-func (s *Server) handleMessage(payload *pb.MessageReq) error {
-	return nil
+func (s *Server) handleMessage(payload *pb.MessageReq, roomIdx uint, c *client) error {
+	return s.app.SendMessge(&dto.MessageDto{
+		RoomIdx:  roomIdx,
+		UserIdx:  c.userIdx,
+		Name:     c.Name,
+		ImageUrl: c.ImageUrl,
+		Message:  payload.Message,
+	})
 }
