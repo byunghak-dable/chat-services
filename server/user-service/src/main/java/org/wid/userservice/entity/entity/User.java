ab1474aea51612b2b098d38f2c94398dd1657172
widcraft <forsythia3240@gmail.com>
2024-01-14 23:44:58 +0900

    [FIX] replace to data lombok annotaion

--- server/user-service/src/main/java/org/wid/userservice/entity/entity/User.java
+++ server/user-service/src/main/java/org/wid/userservice/entity/entity/User.java
@@ -8,13 +8,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "user")
+@Data
 @Builder
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
