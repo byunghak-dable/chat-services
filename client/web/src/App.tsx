237fa46b62954e3dea8fd8cd6f5923070f0a63e1
widcraft <forsythia3240@gmail.com>
2024-01-14 17:09:02 +0900

    [UPDATE] split login page

--- client/web/src/App.tsx
+++ client/web/src/App.tsx
@@ -1,13 +1,24 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
-import GoogleOAuth from "./component/google-signin";
+import Home from "./page/home";
+import Login from "./page/login";
 
 function App() {
+  const isLogin = false;
+
   return (
-    <div className="App">
-      <body className="App-header">
-        <GoogleOAuth />
-      </body>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={isLogin ? <Home /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="/login"
+          element={isLogin ? <Navigate to="/" replace /> : <Login />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/web/src/index.tsx
+++ client/web/src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const rootElement: HTMLElement = document.getElementById("root")!;
+const rootElement = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
