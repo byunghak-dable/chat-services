ff720473e57a4b8950d3ec3aff74e294fb48e777
widcraft <forsythia3240@gmail.com>
2024-01-19 13:40:49 +0900

    [UPDATE] add basic spring-security config
    s

---
+++ server/user-service/src/main/java/org/wid/userservice/service/AuthService.java
@@ -0,0 +1,9 @@
+package org.wid.userservice.service;
+
+import org.springframework.stereotype.Service;
+import org.wid.userservice.port.primary.AuthServicePort;
+
+@Service
+public class AuthService implements AuthServicePort {
+
+}
