af4077642ccc881530bbb23b31812d473f6f3871
widcraft <forsythia3240@gmail.com>
2024-01-19 13:59:50 +0900

    [UPDATE] add validation rule to dto

--- server/user-service/src/main/java/org/wid/userservice/dto/user/RegisterUserDto.java
+++ server/user-service/src/main/java/org/wid/userservice/dto/user/RegisterUserDto.java
@@ -1,11 +1,12 @@
 package org.wid.userservice.dto.user;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 public record RegisterUserDto(
-    @Email String email,
-    String password,
-    String name,
+    @Email @NotNull String email,
+    @NotNull String password,
+    @NotNull String name,
     String gender,
     String thumbnailUrl) {
 }
