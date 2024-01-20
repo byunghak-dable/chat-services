b9c3c93f88e34c5a96d8db19f1327b9884b8287b
widcraft <forsythia3240@gmail.com>
2024-01-20 23:37:10 +0900

    [FIX] validation rule

--- server/user-service/src/main/java/org/wid/userservice/dto/user/RegisterUserDto.java
+++ server/user-service/src/main/java/org/wid/userservice/dto/user/RegisterUserDto.java
@@ -1,12 +1,17 @@
 package org.wid.userservice.dto.user;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record RegisterUserDto(
-    @Email @NotNull String email,
-    @NotNull String password,
-    @NotNull String name,
+    @Email @NotBlank String email,
+    @NotBlank String password,
+    @NotBlank String firstName,
+    @NotBlank String lastName,
     String gender,
     String thumbnailUrl) {
 }
--- server/user-service/src/main/java/org/wid/userservice/dto/user/UserDto.java
+++ server/user-service/src/main/java/org/wid/userservice/dto/user/UserDto.java
@@ -1,9 +1,14 @@
 package org.wid.userservice.dto.user;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record UserDto(
     Long id,
     String email,
-    String name,
+    String firstName,
+    String lastName,
     Boolean gender,
     String thumbnailUrl) {
 }
