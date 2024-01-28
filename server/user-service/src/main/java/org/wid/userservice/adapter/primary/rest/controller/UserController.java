3c06c0943dbb6d0c0459134704ad27ba8ac1c2ba
widcraft <forsythia3240@gmail.com>
2024-01-28 23:15:29 +0900

    [TEMP] test route

--- server/user-service/src/main/java/org/wid/userservice/adapter/primary/rest/controller/UserController.java
+++ server/user-service/src/main/java/org/wid/userservice/adapter/primary/rest/controller/UserController.java
@@ -1,7 +1,11 @@
 package org.wid.userservice.adapter.primary.rest.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wid.userservice.dto.user.UserDto;
@@ -19,5 +23,10 @@ public class UserController {
   @GetMapping("/user/{userId}")
   public Mono<UserDto> getUser(@PathVariable String userId) {
     return userService.getUser(userId);
+  }
+
+  @PostMapping("/user")
+  public Mono<UserDto> getTestUser(@RequestBody Map<String, String> t) {
+    return Mono.just(new UserDto("1", "email", "firstName", t.get("test"), true, "thumnail"));
   }
 }
