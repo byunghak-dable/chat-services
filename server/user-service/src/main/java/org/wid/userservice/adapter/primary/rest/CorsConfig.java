12e362339e66f0a8055c5bb943dc75465c268329
widcraft <forsythia3240@gmail.com>
2024-01-30 00:22:32 +0900

    [UPDATE] use cors web filter

---
+++ server/user-service/src/main/java/org/wid/userservice/adapter/primary/rest/CorsConfig.java
@@ -0,0 +1,28 @@
+package org.wid.userservice.adapter.primary.rest;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public CorsWebFilter corsWebFilter() {
+    CorsConfiguration corsConfig = new CorsConfiguration();
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+    corsConfig.setAllowedOrigins(List.of("*"));
+    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+    corsConfig.setAllowedHeaders(List.of("Content-Type", "Authorization"));
+    // corsConfig.setAllowCredentials(true);
+
+    source.registerCorsConfiguration("/**", corsConfig);
+
+    return new CorsWebFilter(source);
+  }
+}
