bb040e3d40fd253aa8c2a7ce48575f307e6c808a
widcraft <forsythia3240@gmail.com>
2024-01-29 21:47:32 +0900

    [FIX] remove UserDetails implementation

--- server/user-service/src/main/java/org/wid/userservice/entity/entity/User.java
+++ server/user-service/src/main/java/org/wid/userservice/entity/entity/User.java
@@ -1,29 +1,15 @@
 package org.wid.userservice.entity.entity;
 
-import java.util.Collection;
-import java.util.List;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Document(collection = "user")
 @Getter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
-public class User implements UserDetails {
-
-  public enum Role {
-    USER, ADMIN
-  }
+public class User {
 
   public enum LoginType {
     GOOGLE, GITHUB
@@ -34,52 +20,9 @@ public class User implements UserDetails {
 
   private String email;
 
-  private String password;
+  private String name;
 
-  private String firstName;
-
-  private String lastName;
-
-  private Boolean gender;
-
-  private String thumbnailUrl;
-
-  private Role role;
+  private String profile;
 
   private LoginType loginType;
-
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return List.of(new SimpleGrantedAuthority(role.name()));
-  }
-
-  @Override
-  public String getUsername() {
-    return email;
-  }
-
-  @Override
-  public String getPassword() {
-    return password;
-  }
-
-  @Override
-  public boolean isAccountNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isAccountNonLocked() {
-    return true;
-  }
-
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return true;
-  }
-
-  @Override
-  public boolean isEnabled() {
-    return true;
-  }
 }
