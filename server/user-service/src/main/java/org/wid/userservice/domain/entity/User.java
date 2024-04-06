e3bfcb3cc8c1535764c462fd1c7dc64d9780b277
widcraft <forsythia3240@gmail.com>
2024-04-06 10:08:17 +0900

    [FIX] remove lombok from user record

--- server/user-service/src/main/java/org/wid/userservice/domain/entity/User.java
+++ server/user-service/src/main/java/org/wid/userservice/domain/entity/User.java
@@ -1,11 +1,5 @@
 package org.wid.userservice.domain.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@Builder
 public record User(String id, String email, String name, String profile, LoginType loginType) {
   public enum LoginType {
     GOOGLE,
